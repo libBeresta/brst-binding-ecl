56464d443490fe61a8d22d8c911692a672770b04
Dmitry Solomennikov <dmitrys99@mail.ru>
2026-05-06 13:17:59 +0300

    Functions not implemented, hidden
    Signed-off-by: Dmitry Solomennikov <dmitrys99@mail.ru>

--- src/doc_xobject.lisp
+++ src/doc_xobject.lisp
@@ -9,19 +9,19 @@
 
 (include-header)
 
-(func Doc-XObject-CreateFromImage 
-     "BRST_Doc_XObject_CreateFromImage" 
-      XObject 
-  ((pdf Doc)
-   (rect Rect)
-   (image Image)
-   (zoom BOOL)))
+;;(func Doc-XObject-CreateFromImage 
+;;     "BRST_Doc_XObject_CreateFromImage" 
+;;      XObject 
+;;  ((pdf Doc)
+;;   (rect Rect)
+;;   (image Image)
+;;   (zoom BOOL)))
 
-(func Doc-XObject-CreateAsWhiteRect 
-     "BRST_Doc_XObject_CreateAsWhiteRect" 
-      XObject 
-  ((pdf Doc)
-   (rect Rect)))
+;;(func Doc-XObject-CreateAsWhiteRect 
+;;     "BRST_Doc_XObject_CreateAsWhiteRect" 
+;;      XObject 
+;;  ((pdf Doc)
+;;   (rect Rect)))
 
 (func Doc-XObject-Create 
      "BRST_Doc_XObject_Create" 
